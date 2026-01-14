3c31d953db4fc9b5763d38982b76720b7ae21825
ahasana-ncs13 <ahasanancs0485@gmail.com>
2026-01-15 00:52:43 +0600

    added mongodb and apis

---
+++ index.js
@@ -0,0 +1,57 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.j6dmigp.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const BookNestDB = client.db("BookNestDB");
+    const booksCollection = BookNestDB.collection("allbooks");
+
+    app.get("/allbooks", async (req, res) => {
+      const cursor = booksCollection.find({});
+      const allBooks = await cursor.toArray();
+      res.send(allBooks);
+    });
+    app.get("/bookdetails/:id", async (req, res) => {
+        const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const bookDetails = await booksCollection.findOne(query);
+      res.send(bookDetails);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("BookNest server is running");
+});
+
+app.listen(port, () => {
+  console.log(`BookNest server app listening on port ${port}`);
+});
